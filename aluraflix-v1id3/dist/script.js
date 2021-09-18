eff2de7e85f8f5f57ae3e3827cf74448fd109468
Ligiane Vieira <87396681+LigianeVieira@users.noreply.github.com>
2021-09-18 15:41:09 -0300

    Add files via upload

---
+++ aluraflix-v1id3/dist/script.js
@@ -0,0 +1,12 @@
+var listaFilmes = [
+  "https://1.bp.blogspot.com/-E-ixQ5bj0Bg/XQRWyS9ckcI/AAAAAAAAZ9k/3u4BXKDbVfI7WhL1OYU-oMHZOmzyMVoxACLcBGAs/s1600/Ptes%2Ba%2Bvida%2Bsecreta%2Bdos%2Bbichos%2B2.jpg",
+  "https://observatoriodocinema.uol.com.br/wp-content/uploads/2019/07/as-trapaceiras.jpg",
+  "https://2.bp.blogspot.com/-FGNlEiXYnKc/WPdewC0KLuI/AAAAAAAAFYg/NyW3j8jumNIDO6xGQOn8v0-MV88aZZ4TQCLcB/s1600/Um-Senhor-Estagi%25C3%25A1rio.jpg",
+  "https://www.revistabula.com/wp/wp-content/uploads/2019/04/Aquaman-1-610x350.jpg",
+  "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/07/pantera-negra-1200x900-1.jpg",
+  "https://cdn.ome.lt/r0qaGhbow9lij4NKgzbNQYdt-nY=/987x0/smart/uploads/conteudo/fotos/1_ljGEHcV.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
